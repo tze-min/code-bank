8e618444ca199327427ad8f282b91c7420bc5f04
tm <tzemin.koay@gmail.com>
2022-07-24 18:35:05 +0800

    Update 0176-secondHighestSalary.sql

--- leetcode/0176-secondHighestSalary.sql
+++ leetcode/0176-secondHighestSalary.sql
@@ -1,4 +1,19 @@
-# Write your MySQL query statement below
+-- report the second highest salary, but if there's no second highest salary, return null
 
-SELECT max(Salary) AS SecondHighestSalary FROM Employee
-WHERE Salary not IN (SELECT max(Salary) FROM Employee)
+SELECT 
+    MAX(salary) AS SecondHighestSalary
+FROM Employee
+WHERE salary < (SELECT MAX(salary) FROM Employee);
+
+SELECT 
+	MAX(Salary) AS SecondHighestSalary
+FROM Employee
+WHERE Salary NOT IN (SELECT max(Salary) FROM Employee);
+
+-- the below method doesn't return NULL when there's no second highest salary; only returns ""
+
+SELECT 
+    salary AS SecondHighestSalary
+FROM Employee
+ORDER BY salary DESC
+LIMIT 1, 1;
